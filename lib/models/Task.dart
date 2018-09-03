c798ffb4f6834238c679c7d97ee5658bc55ad15d
yeshu <guan.yeshu@gmail.com>
2018-09-03 23:38:39 +0800

    add Task model

---
+++ lib/models/Task.dart
@@ -0,0 +1,37 @@
+import 'package:meta/meta.dart';
+
+class Task {
+  static final tblTask = "Task";
+  static final dbId = "id";
+  static final dbTitle = "title";
+  static final dbStatus = "status";
+
+  String title;
+  int id;
+  TaskStatus tasksStatus;
+
+  Task.create({@required this.title}) {
+    this.tasksStatus = TaskStatus.PENDING;
+  }
+
+  bool operator ==(o) => o is Task && o.id == id;
+
+  //Named constructors
+  Task.update(
+      {@required this.id,
+      @required this.title,
+      this.tasksStatus = TaskStatus.PENDING});
+
+  //Initializer list
+  Task.fromMap(Map<String, dynamic> map)
+      : this.update(
+          id: map[dbId],
+          title: map[dbTitle],
+          tasksStatus: TaskStatus.values[map[dbStatus]],
+        );
+}
+
+enum TaskStatus {
+  PENDING,
+  COMPLETE,
+}
